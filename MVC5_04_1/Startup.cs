ebf353e3cce8b0a23b90d2c935a4289f8ec47438
blvenie <blvenie@gmail.com>
2015-04-23 18:37:23 +0800

    簡易CRUD

---
+++ MVC5_04_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_04_1.Startup))]
+namespace MVC5_04_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
